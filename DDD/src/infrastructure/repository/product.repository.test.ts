6af616265cf63355399b182895782dd920e1cc4f
Fernando Aguilar <fraguilar@pm.me>
2023-11-19 22:27:07 -0300

    feat: adding product repository

--- DDD/src/infrastructure/repository/product.repository.test.ts
+++ DDD/src/infrastructure/repository/product.repository.test.ts
@@ -1,15 +1,15 @@
-import { afterEach, beforeEach, describe, expect, test } from "bun:test";
+import { afterAll, afterEach, beforeAll, beforeEach, describe, expect, test } from "bun:test";
 import {PrismaClient} from "@prisma/client";
 import Product from "../../domain/entity/product/product";
 import ProductRepository from "./product.repository";
 
 describe("product repository test", async () => {
     let prisma: PrismaClient; 
-    beforeEach(async () => {
+    beforeAll(async () => {
         prisma = new PrismaClient();
         await prisma.$connect();
     });
-    afterEach(async () => {
+    afterAll(async () => {
         await prisma.$disconnect();
     });
 
@@ -26,5 +26,59 @@ describe("product repository test", async () => {
             name: "Product 1",
             price: 10
           });
-    })
+    });
+
+    test("should update a product", async () => {
+        const productRepository = new ProductRepository();
+        const product = new Product("2", "Product 1", 10);
+
+        await productRepository.create(product);
+
+        const productModel = await prisma.product.findUnique({
+            where: {id: "2"}
+        });
+        expect(productModel).toStrictEqual({
+            id: "2",
+            name: "Product 1",
+            price: 10
+        });
+        product.changeName("Product 2");
+        product.changePrice(20);
+        await productRepository.update(product);
+        const productModel2 = await prisma.product.findUnique({
+            where: {id: "2"}
+        });
+        expect(productModel2).toStrictEqual({
+            id: "2",
+            name: "Product 2",
+            price: 20
+        });
+    });
+
+    test("should find a product by id", async () => {
+        const productRepository = new ProductRepository();
+        const product = new Product("3", "Product 1", 10);
+        await productRepository.create(product);
+        const productModel = await prisma.product.findUnique({
+            where: {id: "3"}
+        });
+        const foundProduct = await productRepository.findById("3");
+        expect(productModel).toStrictEqual({
+            id: foundProduct.id,
+            name: foundProduct.name,
+            price: foundProduct.price
+        });
+    });
+
+    test("should find all products", async () => {
+        const productRepository = new ProductRepository();
+        const product = new Product("4", "Product 1", 10);
+        await productRepository.create(product);
+        const product2 = new Product("5", "Product 2", 20);
+        await productRepository.create(product2);
+        const productModel = await prisma.product.findMany();
+        const expectedProducts = productModel.map(product => new Product(product.id, product.name, product.price));
+        const foundProducts = await productRepository.findAll();
+        expect(expectedProducts).toEqual(foundProducts)
+    });
 });
--- DDD/src/infrastructure/repository/product.repository.ts
+++ DDD/src/infrastructure/repository/product.repository.ts
@@ -14,17 +14,31 @@ export default class ProductRepository extends PrismaRepository implements Produ
                 name: entity.name,
                 price: entity.price
             }
-        });
-        console.log('response',response);
-        
+        });        
     }
     async update(entity: Product): Promise<void> {
-        throw new Error("Method not implemented.");
+        await this.prisma.product.update({
+            where: {id: entity.id},
+            data: {
+                name: entity.name,
+                price: entity.price
+            }
+        });
     }
     async findById(id: string): Promise<Product> {
-        throw new Error("Method not implemented.");
+        const product = await this.prisma.product.findUnique({
+            where: { id: id }
+        });
+
+        if (product) {
+            return new Product(product.id, product.name, product.price);
+        } else {
+            throw new Error("Product not found.");
+        }
     }
+
     async findAll(): Promise<Product[]> {
-        throw new Error("Method not implemented.");
+        const products = await this.prisma.product.findMany();
+        return products.map(product => new Product(product.id, product.name, product.price));
     }
 }
